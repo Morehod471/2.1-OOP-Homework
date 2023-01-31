28143982e2e15a3f022c6fae7d3711e4529b2a36
Homework2 <ivliev.k@yandex.ru>
2023-01-31 21:22:31 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Car car1 = new Car("null","Granta", -1.7, "Yellow", 2015, "Russia");
+        Car car2 = new Car("AUDI", "A8 50 L TDI quattro", 3.0, "Black", 2020, "Germany");
+        Car car3 = new Car("BMW", "null", 3.0, "Black", 2021, "Germany");
+        Car car4 = new Car("KIA", "Sportage 4 generation", 2.4, "Red", 2018, "South Korea");
+        Car car5 = new Car("Hyundai", "Avante", 1.6, "Orange", -2016, "South Korea");
+
+        System.out.println(car1);
+        System.out.println(car2);
+        System.out.println(car3);
+        System.out.println(car4);
+        System.out.println(car5);
     }
 }
